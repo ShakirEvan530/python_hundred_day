51bd68cfec8ecbb8a375f8fd6d0ca1c92c3274ed
ShakirEvan530 <ausshakirahmed@gmail.com>
2025-04-13 06:28:54 +0600

    11th day done till now

---
+++ harry_100_day.py
@@ -0,0 +1,99 @@
+
+# !target of hundred days of python by code with harry
+
+# !Day 1 Started from code with harry 
+# print("Hello world")
+# print(5)
+# ?Day 1 has been completed
+
+# ! Day2 Starts from here 
+# ?discussed about some basic projects using python . 
+
+# !Day3 starts from here 
+
+# ?import some basic libraries of python
+
+# ! Day4 starts from here 
+# print("Hello world",7)
+# print("Hello World")
+# print(5)
+# print(5*10)
+
+# ? day 4 ends here
+
+# ! Day 5 starts from here 
+
+# print("Hello I am a Cse student\n and my friends are also cse graduates")
+
+# print("Hello my name is \"Evan\" and right now I am a student of Faridpur engineering college Dept of CSE" )
+# print("hey", 6,7 ,sep="/",end= "007\n")
+# print("Evan")
+# learned about how to make any comment and how to make any new line in python
+# ? Day 5 ends here 
+
+# ! Day 6 Starts from here 
+
+# a = 5
+# b = "Evan"
+# d = True
+# c =[1,2,3,4]
+# e = {1,2,3,4}
+# f = (1,2,3,4)
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+
+# ? day 6 ends here 
+
+# !Day 7 starts from here 
+
+# In this tutorial we have learned about creating simple calculat/or using python
+# ? Day 7 ends here 
+
+# ! day 8 starts from here 
+
+# simple calculator creation task
+
+# ? Day 8 ends here 
+
+# ! Day 9 Starts from here 
+
+# simple basic of typecasting in python 
+
+# ?Day 9 ends here 
+
+# !Day 10 starts from here 
+# a = input("Enter your name:")
+# print("My name is",a)
+# x = float(input("Enter the first number:"))
+# y = float(input("Enter the second number:"))
+# z = (x + y)
+# print("The sum of the first two numbers are : ",z)
+# ? Day 10 ends here 
+
+# !Day 11 Starts from here 
+name = "Evan"
+friend = "Shanto"
+anotherFriend="Sakib"
+
+apple= 'He said, "I want to eat an apple"'
+print("Hello," +name)
+print("Hello Lord,"+friend)
+print("Hello Goat,"+anotherFriend)
+print(apple)
+
+
+# ?Day 11 ends here 
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,165 @@
+#string
+
+
+# first_name = "Bro"
+# food = "Pizza"
+# email = 'ausshakirahmed@gmail.com'
+# print(f"Hello {first_name}")
+# print(f"You like {food}")
+# print(f"Your email is  {email}")
+
+# ! integer 
+
+# age = 25;
+# quantity = 3;
+# print(f"You are {age} years old")
+# print(f"You are buying {quantity} items")
+
+
+# !boolean type 
+# is_Student = True
+# if is_Student:
+#     print("you are a student")
+# else:
+#     print("You are not a student")
+
+
+# is_Student2 = False
+# if is_Student2:
+#     print("you are a student")
+# else:
+#     print("You are not a student")
+
+#?typescript 
+# name= "Bro Code"
+# age = 25
+# gpa = 3.2
+# is_Student = True
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+# print(type(is_Student))
+# print(name)
+
+# name = input("What is your name?:")
+# age = int(input("How old are you?:"))
+# age = age+1;
+# print(f"Hello {name}!")
+# print("Happy birthday")
+# print(f"you are {age} years old now")
+
+# item = input("What item would you like to buy?:")
+# price = float(input("What is the price of the item?:"))
+# quantity = int(input("How many would you like to buy?:"))
+# total = price * quantity
+# print(f"You have bought {quantity}  {item}")
+# print(f"Your total is: ${total}")
+
+# !word games 
+# adjective1 = input("Enter an adjective (description):")
+# noun1 = input("Enter a noun(person , place , thing)")
+# adjective2 = input("Enter an adjective(description):")
+# verb1 = input("Enter a verb ending with 'ing")
+# adjective3 = input("Enter an adjective(description):")
+# print(f"Today i went to a {adjective1} zoo.")
+# print(f"In an exhibition, I saw a {noun1}")
+# print(f"{noun1} was {adjective2} and {verb1}")
+# print(f"I was {adjective3}!")
+
+# import math 
+# x = 4 
+# y=9
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(x)
+# print(result)
+# result1 = math.ceil(x)
+# result2 = math.floor(y)
+# print(result1)
+# print(result2)
+
+# radious = float(input("Enter the radious of a circle:"))
+# circumference = 2*math.pi*radious
+# print(f"The circumference is:{round(circumference,2)}")
+
+# age = int(input("Enter your age:"))
+
+# if age >= 18:
+#     print("you are now an adult and you can vote now")
+# elif age <0:
+#     print("You haven't been born yet")
+
+# elif age==0:
+#     print("Your age is zero and you are a baby")
+# elif age>=100:
+#     print("You are very old to give your vote")
+# else:
+#     print("You must be 18+ to sign na and give your vote")
+# *hello world
+
+# !Python calculator
+# operator = input("Enter the operator (+,-,*,/):")
+# num1 = float(input("Enter the first number:"))
+# num2 = float(input("Enter the second number:"))
+
+# if operator =="+":
+#     result = num1 + num2
+#     print("The result is :",result)
+# elif operator =="-":
+#     result = num1 - num2
+#     print("The result is :",result)
+# elif operator =="*":
+#     result = num1 * num2
+#     print("The result is :",result)
+# elif operator =="/":
+#     result = num1 / num2
+#     print("The result is :",result)
+# else: 
+#     print(f"{operator} is not valid operator")
+
+
+# ! kg to pound convettor 
+# weight = float(input("Enter your weight:"))
+# unit = input("kilogram or pounds?(kg/lb):")
+
+# if unit =="kg":
+#     weight = weight * 2.205;
+#     print(f"Your weight in pound is {weight} lb")
+#     unit = "lb"
+# elif unit == "lb":
+#     weight = weight / 2.205;
+#     print (f"Your weight in kilogram is {weight} kg")
+#     unit = "kg"
+# else :
+#     print(f"{unit} is not a valid unit") 
+
+# print(f"Your new weight is {round(weight)} {unit}")
+
+
+# ? kg to pound convertor
+# !weight to pound or pound to weight convertor
+# * kg to pound convertor calculator
+# weight = float(input("Enter your weight:"))
+# unit = input("Kilogram or pound?(kg/lb):")
+
+# if unit=="kg":
+#     weight = weight * 2.205;
+#     print(f"Your weight in pound is {weight} lb")
+#     unit ="lb"
+
+# elif unit=="lb":
+#     weight = weight /2.205;
+#     print(f"Your weight in kilogram is {weight} kg")
+#     unit = "kg"
+
+# else:
+#     print(f"{unit} is not a valid unit")
+
+# print(f"your new weight is {round(weight)} {unit}")
+
+
+
+
+
+
+
